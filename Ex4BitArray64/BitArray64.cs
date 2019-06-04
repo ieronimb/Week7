02cd12eb9f728ab6527d6f4157852f85355f60a1
Birnat Ieronim <Ieronimb@outlook.com>
2019-06-04 09:22:16 +0300

    added exercise 4

---
+++ Ex4BitArray64/BitArray64.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4BitArray64
+{
+        /*4.  64 Bit array
+        Define a class BitArray64 to hold 64 bit values inside an ulong value.
+        Implement IEnumerable<int> and Equals(…), GetHashCode(), [], == and !=
+        BitArray64 simpleArray = new BitArray64();
+        simpleArray.Add(1);
+        simpleArray.Add(2);
+        simpleArray.Add(3);
+        simpleArray.Add(3);
+        Console.WriteLine(simpleArray[2] == simpleArray[3]);
+        foreach (var value in simpleArray)
+        {
+        Console.WriteLine(value);
+        }*/
+
+    class BitArray64 : IEnumerable<int>
+    {
+        //Camp constant
+        private const byte bits = 64;
+        //Camp
+        private ulong number;
+
+        //Cosntructor
+        public BitArray64(ulong number)
+        {
+            this.number = number;
+        }
+        //Proprietati
+        public ulong Number
+        {
+            get { return this.number; }
+            set { this.number = value; }
+        }
+        // [] https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/indexers/using-indexers
+        public byte this[int index] 
+        {
+            get
+            {
+                if (index < 0 || index >= bits)
+                {
+                    throw new ArgumentOutOfRangeException("The index is out of range");
+                }
+               return (byte)(this.Number);
+            }
+            set
+            {
+                if (index < 0 || index >= bits)
+                {
+                    throw new ArgumentOutOfRangeException("The index is out of range");
+                }                          
+            }
+        }
+        //IEnumerable<int>
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < bits; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        //Operator ==
+        public static bool operator == (BitArray64 firstNumber, BitArray64 secondNumber)
+        {
+            return BitArray64.Equals(firstNumber, secondNumber);
+        }
+        //Operator !=
+        public static bool operator !=(BitArray64 firstNumber, BitArray64 secondNumber)
+        {
+            return !(BitArray64.Equals(firstNumber, secondNumber));
+        }
+        //Get Hascode
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() ^ this.Number.GetHashCode(); //https://docs.microsoft.com/en-us/dotnet/api/system.object.gethashcode?view=netframework-4.8
+        }
+
+        //Equals(…)
+        public override bool Equals(object param)
+        {
+            BitArray64 bitArray64 = param as BitArray64;
+            return this.Number.Equals(bitArray64.Number);
+        }
+    }
+}
+
---
+++ Ex4BitArray64/BitArrays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4BitArray64
+{
+    class BitArrays
+    {
+        static void Main(string[] args)
+        {
+            BitArray64 arrayOne = new BitArray64(1157u);           
+            BitArray64 arrayTwo = new BitArray64(15u);            
+
+            Console.WriteLine("\nNumber one == number two?");
+            Console.WriteLine(arrayOne == arrayTwo);
+
+            Console.WriteLine("\nNumber one !- number two?");
+            Console.WriteLine(arrayOne != arrayTwo);
+
+            Console.WriteLine("\nNumber one is equal to itself?");
+            Console.WriteLine(arrayOne.Equals(arrayOne));           
+           
+            Console.ReadKey();
+        }
+    }
+}
